4b54a4531fa01696bb02db4d6c73e049890f50ca
Nyele93 <84391731+Nyele93@users.noreply.github.com>
2022-10-13 18:45:02 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,55 +1,42 @@
 #!/bin/bash
 
-        BASE_DIR=/etc/mariadb/audit_config
+        BASE_DIR=/etc/mariadb_rdba/mariadb_tool
         CONFIGNAME=deploy.conf
         CONFIGFILE=$BASE_DIR/$CONFIGNAME
+        token_key=`cat .token_key`
 
-        if [ "$(which curl 2>/dev/null)" = "" ]
-	then
-		echo "This script requires the curl package. Please install it."
-		exit 1
-	fi
+req_pckg="git ansible"
+which $req_pckg > /dev/null 2>&1
+if [ $? == 0 ]
+then
+echo "Prerequisite packages: [$req_pckg] already installed. Proceeding with setup"
+else 
+   echo "Prerequisite packages not installed. Kindly install below packages to proceed:"
+   echo "[+] git"
+   echo "[+] ansible" 
+
+   exit 1
+fi
+
 
 install_tool(){
+     if [ ! -d $BASE_DIR ]; then 
+        mkdir -p $BASE_DIR
+        cd $BASE_DIR
+        git clone https://$token_key@github.com/Nyele93/Tools_to_push.git
 
-    cp -u audit_report_performanc audit.yml deploy.conf .token_key $BASE_DIR/
-    cp -u -R performance_audit/ $BASE_DIR/
-
-    cp -u audit_report_performance audit.yml deploy.conf .token_key /usr/local/sbin/
-    cp -R performance_audit/ /usr/local/sbin
- 
-   echo "Setup Process completed. please confirm files in $BASE_DIR"
+        echo "Setup Process completed. please confirm files in $BASE_DIR and adjust config file accordingly."
+    else 
+           echo "Installation already existing. Check base directory: $BASE_DIR" 
+    fi 
 }
 
-build_config()
-{
-	if [ "$LOGNAME" != "root" ]
-	then
-		CONFIGFILE=$BASE_DIR/$CONFIGNAME
-	else
-		[[ ! -d $BASE_DIR ]] && mkdir $BASE_DIR
-	fi
+cleanup_dir(){
+     rm -f $BASE_DIR/Tools_to_push/setup.sh
+     rm -f $BASE_DIR/Tools_to_push/README.md
+}
 
-	if [ -f $CONFIGFILE ]
-	then
-		echo "Config file: '$CONFIGFILE' already exists, no overwrite of current config."
-        else 
-             cp ./$CONFIGNAME $BASE_DIR/$CONFIGNAME
-             echo "Created '$CONFIGFILE'. Please adjust it according to your setup."
-	fi
-
-           if  [[ ! -f $BASE_DIR/audit_performance ]]
-                 then
-                   install_tool
-            else
-                echo "Tool seems to have been installed. Please check Base Directory."
-           fi
-   
-	exit 0
-	}
-
-build_config; 
+install_tool;
+cleanup_dir;
 
 exit 0
-
-
